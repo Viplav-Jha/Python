4099be0f2734fddd453e844e9dff34a61dd8cce1
Viplav jha <viplavjha1993@gmail.com>
2024-02-01 12:03:07 +0530

    password generator logics for random number

--- passwordGenerator.py
+++ passwordGenerator.py
@@ -1,4 +1,18 @@
 import random
 
-r = random.randrange(-5, 11)
-print(r)
+top_of_range =input("Type a number: ")
+
+
+# is digit return true or false
+if top_of_range.isdigit(): 
+    top_of_range =int(top_of_range)
+
+    if top_of_range <= 0:
+        print('Please type a number larger then 0 next time')
+        quit()
+else:
+    print('Please type a number next time. ')
+    quit()
+
+random_number = random.randint(0, top_of_range)
+print(random_number);
