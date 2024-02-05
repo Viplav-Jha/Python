c65db67751e7ac427c3b1c0b8af8cd1f290846ad
Viplav jha <viplavjha1993@gmail.com>
2024-02-05 22:11:52 +0530

    encryption logic and decrytion logic added

--- passwordManager.py
+++ passwordManager.py
@@ -1,11 +1,21 @@
 from cryptography.fernet import Fernet
 
-master_pwd =input("What is the master passowrd? ")
-
+'''
 def write_key():
     key = Fernet.generate_key()
     with open("key.key","wb") as key_file:
-        key_file.write(key)
+        key_file.write(key)'''
+
+def load_key():
+    file =open("key.key","rb")
+    key =file.read()
+    file.close()
+    return key
+
+
+
+key =load_key() 
+fer = Fernet(key)
 
 
 def view():
@@ -14,7 +24,7 @@ def view():
             data = line.strip().split("|")
             if len(data) == 2:
                 user, passw = data
-                print("User:", user, "| Password:", passw)
+                print("User:", user, "| Password:", fer.decrypt(passw.encode()).decode())
             else:
                 print("Invalid data format in the file:", line)
 
@@ -24,7 +34,7 @@ def add():
     pwd  =  input("Password: ")
 
     with open('passwords.txt','a') as f:
-        f.write(name + "|" + pwd + "\n")
+        f.write(name + "|" + fer.encrypt(pwd.encode()).decode() + "\n")
 
 while True:
     mode = input("would you like to add  a new password or view existing ones (view/add)? , press q to quit? ").lower();
